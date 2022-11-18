2b6027fa558d5a490720e990c4c5f4d960c85096
mhesselbarth <mhk.hesselbarth@gmail.com>
2022-11-18 09:48:59 -0500

    Adding mean

--- scripts/Caribbean-C-storage-range.R
+++ scripts/Caribbean-C-storage-range.R
@@ -9,7 +9,7 @@ agb_sd <- 28.98 #standard deviation of mean ag biomass
 bgb_mean <- 191.47 #mean belowground (bg) biomass (gDW m-2)
 bgb_sd <- 135.67 #standard deviation of mean bg biomass
 
-n_sample <- 100
+n_sample <- 1000000 
 
 # create normal distribution values using mean and sd
 df_range <- purrr::map_dfr(list(ag = c(agb_mean, agb_sd), bg = c(bgb_mean, bgb_sd)), function(i) {
@@ -23,16 +23,20 @@ df_range <- purrr::map_dfr(list(ag = c(agb_mean, agb_sd), bg = c(bgb_mean, bgb_s
   # sample values from lower and upper for each cell, we might need to to this in Rcpp
   n_cells <- 14340940
   
+  # calculate sum of all cells for low mean and high
   total_lower <- sum(sample(x = samples[samples < lower], size = n_cells, replace = TRUE))
   
+  total_mean = sum(sample(x = samples, size = n_cells, replace = TRUE))
+
   total_higher <- sum(sample(x =  samples[samples > upper], size = n_cells, replace = TRUE))
   
-  data.frame(lower = total_lower, higher = total_higher)
+  data.frame(lower = total_lower, mean = total_mean, higher = total_higher)
   
 }, .id = "part")
 
 
 # tidyr::pivot_longer(df_range, -part, names_to = "range") |> 
-ggplot(df_range, aes(x = factor(part), ymin = lower, ymax = higher)) + 
-  geom_errorbar(linewidth = 1, width = 0.25) + 
+ggplot(df_range, aes(x = factor(part))) + 
+  geom_errorbar(aes(ymin = lower, ymax = higher), linewidth = 1, width = 0.25) + 
+  geom_point(aes(y = mean), shape = 19, size = 5) +
   theme_classic()
