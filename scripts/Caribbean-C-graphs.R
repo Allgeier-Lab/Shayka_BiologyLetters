b8bf24376db66a401ac254ca66038694a3b6a1d1
bshayka <bshayka@umich.edu>
2022-11-30 19:36:14 -0500

    Modified graph script to use new csv files from updated C range calculation scripts. The graph code still needs to be updated

--- scripts/Caribbean-C-graphs.R
+++ scripts/Caribbean-C-graphs.R
@@ -15,92 +15,96 @@ library(ggbreak) #S Xu, M Chen, T Feng, L Zhan, L Zhou, G Yu. Use ggbreak to eff
 Cseagrass <- read_csv("Results/seagrass-carbon.csv")
 Csediment <- read_csv("Results/sediment-carbon.csv")
 
+#get names of all tif database
+countries_attr_tables <- list.files(path = "data/caribbean_maps", pattern = "*.tif.vat.dbf$", full.names = TRUE, recursive = TRUE) |> 
+  stringr::str_sort()
 
-countries <- c("Bahamas", "British Virgin Islands", "Cayman Islands", "Cuba", "Dominican Republic", "Anguilla", "Antigua & Barbuda", "Dominica", 
-               "Guadeloupe", "Montserrat", "Saba", "St. Barthelemy", "St. Martin", "St. Eustatius", "St. Maarten", "St. Kitts & Nevis", 
-               "Barbados", "Grenada", "Martinique", "St. Lucia", "St. Vincent & the Grenadines", "Trinidad & Tobago", "Haiti", "Jamaica", 
-               "Puerto Rico", "Turks & Caicos", "US Virgin Islands")
+#create table with number of sparse and dense cells
+areas <- purrr::map_dfr(countries_attr_tables, function(tab_i) {
+  tab_temp <- foreign::read.dbf(tab_i) #rat=raster attribute table #import rat from database
+  n_cells <- ifelse(test = length(tab_temp$Count[tab_temp$Value == "12"]) == 0, 
+                   yes = 0, no = tab_temp$Count[tab_temp$Value == "12"]) + 
+    ifelse(test = length(tab_temp$Count[tab_temp$Value == "13"]) == 0, 
+           yes = 0, no = tab_temp$Count[tab_temp$Value == "13"])
+  data.frame(n_cells = n_cells, country = stringr::str_split(tab_i, pattern = "/", 
+                                                           simplify = TRUE)[, 3])}) %>%
+  mutate(area = n_cells * 16) #each grid cell is 4m x 4m = 16 m2 area
+
+countries <- c("Anguilla", "Antigua & Barbuda", "Barbados", "Bahamas", "British Virgin Islands", "Cuba", "Cayman Islands", "Dominica", 
+               "Dominican Republic", "Grenada", "Guadeloupe", "Haiti", "Jamaica", "Martinique", "Montserrat", "Puerto Rico", "Saba", 
+               "St. Barthelemy", "St. Lucia", "St. Martin", "St. Eustatius", "St. Maarten", "St. Kitts & Nevis", 
+               "St. Vincent & the Grenadines", "Turks & Caicos", "Trinidad & Tobago", "US Virgin Islands")
 
 
 ##Data analysis -------------
-##C totals for the Caribbean and #C breakdown by AG, BG, and Sediment for Caribbean
+##C totals for the Caribbean and C breakdown by AG, BG, and Sediment for Caribbean
 Carib_Cseagrass <- Cseagrass %>%
-  group_by(part, range, country) %>%
+  group_by(part, type, range, country) %>%
   summarise(means = mean(value)) %>%
+  group_by(part, range, country) %>%
+  summarise(typesums = sum(means)) %>%
   group_by(part,range) %>%
-  summarise(sums = sum(means)) %>%  #units of gC m-2
-  mutate(TgC = (sums*16)/1000000000000)
+  summarise(sums = sum(typesums)) %>%  #units of gC m-2
+  mutate(TgC = (sums*16)/1000000000000) #units of TgC
 
 Carib_Csediment <- Csediment %>%
   group_by(type, range, country) %>%
   summarise(means = mean(value)) %>%
   group_by(type,range) %>%
   summarise(sums = sum(means)) %>% #units of gC m-2
-  mutate(Tg = (sums*16)/1000000000000)
-
-###################
-
-Caribbean_Csums_mat <- Reduce('+', Cresult_list) #first line is the sum of all the first lines of the matrices in the list, second line is...
-Caribbean_Ctotals <- colMeans(Caribbean_Csums_mat)
-Caribbean_Csds <- apply(Caribbean_Csums_mat, 2, sd) #2 means apply by columns (1 would be by rows), sd is standard deviation function
-
-Caribbean_carbon <- sum(Caribbean_Ctotals[c(1:3)])
-Caribbean_carbon_sd <- sapply(Caribbean_Csds[c(1:3)], function(x) x^2 ) %>%
-  sum() %>%
-  sqrt()
-
+  mutate(TgC = (sums*16)/1000000000000) %>% #units of TgC
+  mutate(part = type)
 
 #C AG,BG,Sed
-totals <- rbind(Caribbean_Ctotals, Caribbean_Ntotals, Caribbean_Ptotals)
-rownames(totals) <- c("Carbon", "Nitrogen", "Phosphorus")
-colnames(totals) <- c("Aboveground", "Belowground", "Sediment", "Area")
-totals_df <- data.frame(totals)
+Carib_Ctotals <- rbind(Carib_Cseagrass, Carib_Csediment) %>%
+  select(part, range, TgC) %>%
+  pivot_wider(names_from = range, values_from = TgC)
 
-sds <- rbind(Caribbean_Csds, Caribbean_Nsds, Caribbean_Psds)
-rownames(sds) <- c("Carbon", "Nitrogen", "Phosphorus")
-colnames(sds) <- c("ag_sd", "bg_sd", "sed_sd", "area_sd")
-sds_df <- data.frame(sds)
-
-cnp <- c(Caribbean_carbon, Caribbean_nitrogen, Caribbean_phosphorus)
-cnp_sd <- c(Caribbean_carbon_sd, Caribbean_nitrogen_sd, Caribbean_phosphorus_sd)
-
-cnp_df <- cbind(totals_df, sds_df) #this combines the two dfs by columns and doesn't overlap any of them like the join functions do
-cnp_df$Total <- cnp
-cnp_df$total_sd <- cnp_sd
-
-cnp_df <- cnp_df %>%
-  mutate(ag_per = (Aboveground/Total) * 100,
-         bg_per = (Belowground/Total) * 100,
-         sed_per = (Sediment/Total) * 100, #these give the % of the total C, N, or P that each component (AG, BG, and Sed) represents
-         nutrient = c("Carbon", "Nitrogen", "Phosphorus")) 
-
-
+Caribbean_C <- sum(Carib_Ctotals$mean)
+Caribbean_C_low <- sum(Carib_Ctotals$low)
+Caribbean_C_high <- sum(Carib_Ctotals$high)
 
 ##Value of C and seagrass by country
-Cmeans_list <- lapply(Cresult_list, function(x) colMeans(x))
-Cmeans_matrix <- matrix(unlist(Cmeans_list), ncol=4, byrow=T)
-Cmeans_df <- data.frame(Cmeans_matrix)
-Cbycountry <- cbind(countries, Cmeans_df) %>%
-  rename(ag_c = X1,
-         bg_c = X2,
-         sed_c = X3,
-         area = X4) %>%
-  mutate(total_c = (ag_c + bg_c + sed_c), 
-         c_value = (total_c * (18*(44/12)*1000000))/1000000000, #all carbon columns are in Tg, and carbon value is in $/tCO2e (1000000 tonnes = 1 Tg) #CO2 has an atomic mass of 44 #end value is in $ 
+Cseagrassbycountry <- Cseagrass %>%
+  group_by(part, type, range, country) %>%
+  summarise(means = mean(value)) %>%
+  group_by(part, range, country) %>%
+  summarise(typesums = sum(means)) %>%
+  filter(range == "mean") %>%
+  group_by(country) %>%
+  summarise(sums = sum(typesums)) %>%  #units of gC m-2
+  mutate(grassTgC = (sums*16)/1000000000000) %>% #units of TgC
+  select(country, grassTgC)
+  
+Csedimentbycountry <- Csediment %>%
+  group_by(type, range, country) %>%
+  summarise(means = mean(value)) %>%
+  filter(range == "mean") %>% #units of gC m-2
+  mutate(sedTgC = (means*16)/1000000000000) %>% #units of TgC
+  ungroup() %>%
+  select(country, sedTgC)
+  
+Ctotalbycountry <- left_join(Cseagrassbycountry, Csedimentbycountry, by = "country") %>%
+  left_join(areas, by = "country") %>%
+  cbind(countries) %>%
+  mutate(TgC = grassTgC + sedTgC,
+         c_value = (TgC * (18*(44/12)*1000000))/1000000000, #carbon is in Tg, and carbon value is in $/tCO2e (1000000 tonnes = 1 Tg) #CO2 has an atomic mass of 44 #end value is in $ 
                                   #https://carbonpricingdashboard.worldbank.org/map_data #California cap and trade $18 for 2021 
          seagrass_value = ((area/10000) * 28916)/1000000000) #28916 $/ha/yr in 2011 from Costanza et al. 2014 #area is in m2 #so end value is in $/yr
                           #dividing by 1,000,000,000 gives the final number in billions of dollars!!
 
-sum(Cbycountry$seagrass_value)
-sum(Cbycountry$c_value)
-sum(Cbycountry$area)
+sum(Ctotalbycountry$seagrass_value)
+sum(Ctotalbycountry$c_value)
+sum(Ctotalbycountry$area)
 
-byCountry <- format(Cbycountry, scientific = F, digits = 3) #if you don't want the numbers in scientific notation
+byCountry <- format(Ctotalbycountry, scientific = F, digits = 3) #if you don't want the numbers in scientific notation
+
 
 ##C stock in Caribbean compared to...
 #all of these are in Petagrams (Pg)
-CaribCPg <- Caribbean_carbon/1000
-CaribCsdPg <- Caribbean_carbon_sd/1000
+CaribCPg <- Caribbean_C/1000
+CaribCPgup <- Caribbean_C_high/1000
+CaribCPglow <- Caribbean_C_low/1000
 GlobalSG <- (4.2+8.4)/2
 GlobalSGrange <- GlobalSG-4.2
 MedSG <- ((1235.39+1733.29)/2)/1000
@@ -118,8 +122,8 @@ Fishlowuncert <- 0.7/8
 
 
 values <- c(CaribCPg, GlobalSG, MedSG, Amazon, Tempforest, Fish)
-upuncerts <- c(CaribCsdPg, GlobalSGrange, MedSGrange, Amazonuncert, Tempforestsd, Fishupuncert)
-lowuncerts <- c(CaribCsdPg, GlobalSGrange, MedSGrange, Amazonuncert, Tempforestsd, Fishlowuncert)
+upuncerts <- c(CaribCPgup, GlobalSGrange, MedSGrange, Amazonuncert, Tempforestsd, Fishupuncert)
+lowuncerts <- c(CaribCPglow, GlobalSGrange, MedSGrange, Amazonuncert, Tempforestsd, Fishlowuncert)
 names <- c("Caribbean seagrass", "Global seagrass", "Mediterranean seagrass", "Amazon woody biomass", "Temperate forests", "Global fish")
 
 
@@ -134,28 +138,30 @@ comparisons_df <- data.frame(names, values, upuncerts, lowuncerts) %>%
 
 
 ##Graphs ---------------- 
-#C, N, and P totals for the Caribbean
+#C breakdown by AG, BG, and Sediment for Caribbean
 
-mb <- unique(as.numeric(1:10 %o% 10 ^ (0:3))) #this object creates a set of minor break values that are reasonable for log scales up to 10,000
-cnp_graph2 <- cnp_df %>%
-  ggplot(aes(x=nutrient, y=Total)) +
-  geom_col() +
-  geom_errorbar(aes(ymin=Total-total_sd, ymax=Total+total_sd), width=.2) +
-  scale_y_continuous(trans='log10',
-                     limits = c(NA,2000),
-                     breaks = scales::breaks_log(),
-                     minor_breaks = mb) + #this pulls minor breaks from the object above
-  theme_classic() +
-  theme(panel.grid.major.y = element_line(color = "darkgray"),
-        panel.grid.minor.y = element_line(),
-        aspect.ratio = 2/1) +  #makes the x axis narrower but keeps the height the same
-  labs(x= "Nutrient",
-       y= expression('Amount in Caribbean (Tg) [Ticks placed on '*log[10]*' scale]')) + 
-  theme(axis.text = element_text(size=12),
-        axis.title = element_text(size=12, color = "black")) +
-  geom_text(aes(label = c("1336.3\n(0.4%)", "109.5\n(0.2%)", "7.2\n(0.05%)"), y = Total - (Total*0.3)), color="white") #\n makes line breaks
-
-ggsave(filename = "Caribbean_cnp_log10.pdf", path="outputs", plot=cnp_graph2, device = "pdf", width = 7, height = 6, units="in", dpi=300)
+# #C, N, and P totals for the Caribbean
+# 
+# mb <- unique(as.numeric(1:10 %o% 10 ^ (0:3))) #this object creates a set of minor break values that are reasonable for log scales up to 10,000
+# cnp_graph2 <- cnp_df %>%
+#   ggplot(aes(x=nutrient, y=Total)) +
+#   geom_col() +
+#   geom_errorbar(aes(ymin=Total-total_sd, ymax=Total+total_sd), width=.2) +
+#   scale_y_continuous(trans='log10',
+#                      limits = c(NA,2000),
+#                      breaks = scales::breaks_log(),
+#                      minor_breaks = mb) + #this pulls minor breaks from the object above
+#   theme_classic() +
+#   theme(panel.grid.major.y = element_line(color = "darkgray"),
+#         panel.grid.minor.y = element_line(),
+#         aspect.ratio = 2/1) +  #makes the x axis narrower but keeps the height the same
+#   labs(x= "Nutrient",
+#        y= expression('Amount in Caribbean (Tg) [Ticks placed on '*log[10]*' scale]')) + 
+#   theme(axis.text = element_text(size=12),
+#         axis.title = element_text(size=12, color = "black")) +
+#   geom_text(aes(label = c("1336.3\n(0.4%)", "109.5\n(0.2%)", "7.2\n(0.05%)"), y = Total - (Total*0.3)), color="white") #\n makes line breaks
+# 
+# ggsave(filename = "Caribbean_cnp_log10.pdf", path="outputs", plot=cnp_graph2, device = "pdf", width = 7, height = 6, units="in", dpi=300)
 
 
 #Value of C and seagrass by country
